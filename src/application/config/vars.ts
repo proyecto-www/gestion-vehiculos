1140dee0f3c1acfbd4ecc3bcbd84a65788ad8f02
David Velasco <velasco.david@correounivalle.edu.co>
2022-12-05 15:39:00 -0500

    Variables de entorno y quitar dynamo

--- src/application/config/vars.ts
+++ src/application/config/vars.ts
@@ -2,5 +2,10 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 
 export const env = {
-    TABLE_NAME: process.env.TABLE_NAME as string
+RDS_USER :process.env.RDS_USER,
+RDS_HOST:process.env.RDS_HOST,
+RDS_DATABASE:process.env.RDS_DATABASE,
+RDS_PASSWORD:process.env.RDS_PASSWORD,
+RDS_PORT:parseInt(process.env.RDS_PORT as string),
+
 }
